d07a0c7367e4d5e82dde8036eec6bc517debcad2
huub@mail.ru <huub@mail.ru>
2021-09-30 21:57:37 +0300

    add class UserRepositoriImpl and @Repository

--- src/main/java/com/example/med/repository/UserRepository.java
+++ src/main/java/com/example/med/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.med.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.med.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
 }
