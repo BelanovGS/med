bc4da001a2ee8fdee31d157344356ed622e3039e
huub@mail.ru <huub@mail.ru>
2021-10-01 14:23:58 +0300

    UserRepositoryImpl creation

--- src/main/java/com/example/med/repository/UserRepository.java
+++ src/main/java/com/example/med/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.med.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.med.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
 
     String getRole();
---
+++ src/main/java/com/example/med/repository/UserRepositoryImpl.java
@@ -0,0 +1,79 @@
+package com.example.med.repository;
+
+import com.example.med.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository{
+    @Override
+    public String getRole() {
+        return null;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(Integer integer) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<User> findAllById(Iterable<Integer> integers) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(User entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
